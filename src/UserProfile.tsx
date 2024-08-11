9b285bc829bd20fe37f0c97b653db7db73acae63
David Freixes <dfis1996@gmail.com>
2024-08-11 11:57:37 +0200

    implementation of the rating component

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -1,6 +1,7 @@
 import Person2Icon from '@mui/icons-material/Person2';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import EventCard from './components/EventCardComponent';
 
@@ -8,7 +9,7 @@ const userData = {
   username: 'johnDoe',
   firstName: 'John',
   email: 'johndoe@example.com',
-  occupation: 'Software Engineer',
+  rating: 4.5,
 };
 
 function UserProfile() {
@@ -31,9 +32,12 @@ function UserProfile() {
               <Typography variant="body1">
                 {userData.email}
               </Typography>
-              <Typography variant="body1">
-                {userData.occupation}
-              </Typography>
+              <Rating
+                name="read-only"
+                value={userData.rating}
+                precision={0.5}
+                readOnly
+              />
             </Grid>
 
             <Grid item xs={2} sx={{ marginLeft: 'auto' }}>
@@ -54,7 +58,7 @@ function UserProfile() {
           </Grid>
         </Grid>
         <Grid item xs={20}>
-          <Grid container spacing={3}>
+          <Grid container spacing={25}>
             <Grid item xs={6}>
               <Typography variant="h6" textAlign="center">
                 Eventos Creados
