78cc830abd88f41d7ddf8bd97a4ea4218e8faa48
Jaranag <joel.aranag@gmail.com>
2024-08-20 09:53:00 +0200

    added apiGetSports function

--- src/api/SpentApiManager.ts
+++ src/api/SpentApiManager.ts
@@ -1,19 +1,33 @@
-import { User } from "../types/types";
+import { Sport, User } from "../types/types";
 import SpentApi from "./SpentApi";
 
-
 export const apiGetUser = async (username: string): Promise<User> => {
-    const response = await SpentApi.get(`/${username}`,{headers: {'authorization': localStorage.getItem('accessToken')}});
-    console.log(response);
-    const userData = response.data;
-    const user: User = {
-        id: userData.id,
-        username: userData.username,
-        email: userData.email,
-        firstName: userData.firstName,
-        rating: userData.rating,
-        eventsCreated: userData.eventsCreated,
-        joinedEvents: userData.joinedEvents
-    };
-    return user;
+  const response = await SpentApi.get(`/${username}`, {
+    headers: { authorization: localStorage.getItem("accessToken") },
+  });
+  console.log(response);
+  const userData = response.data;
+  const user: User = {
+    id: userData.id,
+    username: userData.username,
+    email: userData.email,
+    firstName: userData.firstName,
+    rating: userData.rating,
+    eventsCreated: userData.eventsCreated,
+    joinedEvents: userData.joinedEvents,
+  };
+  return user;
+};
+
+export const apiGetSports = async (): Promise<Sport[]> => {
+  const response = await SpentApi.get(`/sports`, {
+    headers: { authorization: localStorage.getItem("accessToken") },
+  });
+  const apiSports = response.data;
+  const sports: Sport[] = apiSports.map((sport: any) => ({
+    id: sport.id,
+    sportName: sport.name,
+    description: sport.description,
+  }));
+  return sports;
 };
